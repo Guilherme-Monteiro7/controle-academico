cfbe72f5b25c35f922e4097008d846a824698913
Guilherme Monteiro <monteiroguilherme1627@gmail.com>
2026-08-13 20:41:03 -0300

    fix(regex): resolve vulnerabilidade de catastrophic backtracking no validaEmail

--- controle-academico/src/regex/ValidaCampos.java
+++ controle-academico/src/regex/ValidaCampos.java
@@ -1,60 +1,69 @@
 package regex;
 
 public class ValidaCampos {
-    public static boolean validaNome(String nome){
+
+    public static boolean validaNome(String nome) {
         return nome.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
     }
-    public static boolean validaEndereco(String endereco){
+
+    public static boolean validaEndereco(String endereco) {
         return endereco.matches("\\p{Upper}[\\p{IsLatin}\\p{Alnum}[ ][,-]]+");
     }
-    public static boolean validaEmail(String mail){
-        return mail.matches("([\\p{Alnum}\\._-])+@([\\p{Alnum}])+(\\.([\\p{Alnum}])+)*");
+
+    public static boolean validaEmail(String email) {
+        // Refatorado para evitar Catastrophic Backtracking (Estouro de pilha/StackOverflow)
+        // Grupos de captura não-gulosos e sem quantificadores sobrepostos.
+        return email.matches("[\\p{Alnum}._-]+@[\\p{Alnum}]+(?:\\.[\\p{Alnum}]+)*");
     }
-    public static boolean validaFone(String fone){
+
+    public static boolean validaFone(String fone) {
         return fone.matches("\\(\\p{Digit}{2}\\)\\s\\p{Digit}{4,5}-\\p{Digit}{4}");
     }
-    public static boolean validaMatricula(String matr){
+
+    public static boolean validaMatricula(String matr) {
         return matr.matches("\\p{Alnum}+");
     }
-    public static boolean validaCurso(String curso){
+
+    public static boolean validaCurso(String curso) {
         return curso.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
     }
-    
+
     //////////////////////////////////////////////////////////
-    
-    public static boolean validaNomeDisciplina(String nome){
+
+    public static boolean validaNomeDisciplina(String nome) {
         return nome.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
     }
-    public static boolean validaCodigoDisciplina(String code){
-        return code.matches("\\p{Alnum}+");
+
+    public static boolean validaCodigoDisciplina(String codigo) {
+        return codigo.matches("\\p{Alnum}+");
     }
-    public static boolean validaCargaHoraria(String ch){
+
+    public static boolean validaCargaHoraria(String ch) {
         return ch.matches("\\p{Digit}+");
     }
-    
+
     //////////////////////////////////////////////////////////
-    
-    public static boolean validaAno(String ano){
+
+    public static boolean validaAno(String ano) {
         return ano.matches("\\p{Digit}{4}");
     }
-    public static boolean validaNota(String n){
+
+    public static boolean validaNota(String n) {
         return n.matches("\\p{Digit}{1,2}(\\.\\p{Digit}{1,2})?");
     }
-    public static boolean validaFaltas(String f){
+
+    public static boolean validaFaltas(String f) {
         return f.matches("\\p{Digit}+");
     }
-    
+
     /////////////////////////////////////////////////////////
-    
-    public static boolean validaFormacao(String form){
-        return form.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
+
+    public static boolean validaFormacao(String forma) {
+        return forma.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
     }
-    public static boolean validaSalario(String sal){
+
+    public static boolean validaSalario(String sal) {
         return sal.matches("\\p{Digit}{1,10}(\\.\\p{Digit}{1,3})?");
     }
-    
-    ////////////////////////////////////////////////////////
-    
-    
-    
+
 }
