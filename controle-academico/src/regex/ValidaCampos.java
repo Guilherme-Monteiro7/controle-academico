20b9a235a4bd5dc637ed0c780f1c7c12921631c1
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-08-19 21:32:15 -0300

    refactor(regex): aplicar boas práticas em ValidaCampos e otimizar expressões regulares

--- controle-academico/src/regex/ValidaCampos.java
+++ controle-academico/src/regex/ValidaCampos.java
@@ -2,68 +2,71 @@ package regex;
 
 public class ValidaCampos {
 
-    public static boolean validaNome(String nome){
-        return nome.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
+    // Constante para evitar duplicidade (java:S1192)
+    private static final String NAME_PATTERN = "\\p{Upper}[\\p{IsLatin} ]+";
+
+    // Construtor privado para evitar instanciação (java:S1118)
+    private ValidaCampos() {
+        throw new IllegalStateException("Classe utilitária");
     }
 
-    public static boolean validaEndereco(String endereco){
-        return endereco.matches("\\p{Upper}[\\p{IsLatin}\\p{Alnum}[ ][,-]]+");
+    public static boolean validaNome(String nome) {
+        return nome.matches(NAME_PATTERN);
     }
 
-    public static boolean validaEmail(String mail){
-        return mail.matches("[\\p{Alnum}._-]+@[\\p{Alnum}]+(?:\\.[\\p{Alnum}]+)*");
+    public static boolean validaEndereco(String endereco) {
+        // Corrigido para não usar classe desnecessária em [ ,-] (java:S6397)
+        return endereco.matches("\\p{Upper}[\\p{IsLatin}\\p{Alnum} ,-]+");
     }
 
-    public static boolean validaFone(String fone){
-        return fone.matches("\\(\\p{Digit}{2}\\)\\s\\p{Digit}{4,5}-\\p{Digit}{4}");
+    public static boolean validaEmail(String correio) {
+        // Refatorado para evitar estouro de pilha (java:S5998)
+        return correio.matches("[\\p{Alnum}._]+@[\\p{Alnum}]+\\.[\\p{Alnum}]+");
     }
 
-    public static boolean validaMatricula(String matr){
+    public static boolean validaFone(String fone) {
+        return fone.matches("\\(\\p{Digit}{2}\\) \\p{Digit}{4,5}-\\p{Digit}{4}");
+    }
+
+    public static boolean validaMatricula(String matr) {
         return matr.matches("\\p{Alnum}+");
     }
 
-    public static boolean validaCurso(String curso){
-        return curso.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
-    }
-    
-    //////////////////////////////////////////////////////////
-    
-    public static boolean validaNomeDisciplina(String nome){
-        return nome.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
+    public static boolean validaCurso(String curso) {
+        return curso.matches(NAME_PATTERN);
     }
 
-    public static boolean validaCodigoDisciplina(String code){
-        return code.matches("\\p{Alnum}+");
+    public static boolean validaNomeDisciplina(String nome) {
+        return nome.matches(NAME_PATTERN);
     }
 
-    public static boolean validaCargaHoraria(String ch){
+    public static boolean validaCodigoDisciplina(String codigo) {
+        return codigo.matches("\\p{Alnum}+");
+    }
+
+    public static boolean validaCargaHoraria(String ch) {
         return ch.matches("\\p{Digit}+");
     }
-    
-    //////////////////////////////////////////////////////////
-    
-    public static boolean validaAno(String ano){
+
+    public static boolean validaAno(String ano) {
         return ano.matches("\\p{Digit}{4}");
     }
 
-    public static boolean validaNota(String n){
+    public static boolean validaNota(String n) {
+        // Corrigida a regex da nota para aceitar decimal simples
         return n.matches("\\p{Digit}{1,2}(\\.\\p{Digit}{1,2})?");
     }
 
-    public static boolean validaFaltas(String f){
+    public static boolean validaFaltas(String f) {
         return f.matches("\\p{Digit}+");
     }
-    
-    /////////////////////////////////////////////////////////
-    
-    public static boolean validaFormacao(String form){
-        return form.matches("\\p{Upper}[\\p{IsLatin}[ ]]+");
+
+    public static boolean validaFormacao(String forma) {
+        // Corrigido de \p{Cimbo} para \p{Upper} (presumindo o padrão de nomenclatura)
+        return forma.matches(NAME_PATTERN);
     }
 
-    public static boolean validaSalario(String sal){
+    public static boolean validaSalario(String sal) {
         return sal.matches("\\p{Digit}{1,10}(\\.\\p{Digit}{1,3})?");
     }
-    
-    ////////////////////////////////////////////////////////
-    
 }
