466b4277fd87365d9b33145946d44a0238019986
Marilia da Silva Feitosa <marilia.feitosa.contato@gmail.com>
2026-08-24 23:30:48 -0300

    refactor(view): simplificar a inicialização de botões e otimizar manipuladores de eventos em ViewPrincipal

--- controle-academico/src/view/ViewPrincipal.java
+++ controle-academico/src/view/ViewPrincipal.java
@@ -27,36 +27,24 @@ public class ViewPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnProfessores = new javax.swing.JButton();
-        btnAlunos = new javax.swing.JButton();
-        btnDisciplinas = new javax.swing.JButton();
+        javax.swing.JButton btnProfessores = new javax.swing.JButton();
+        javax.swing.JButton btnAlunos = new javax.swing.JButton();
+        javax.swing.JButton btnDisciplinas = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
         btnProfessores.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/user.png"))); // NOI18N
         btnProfessores.setText("Professores");
-        btnProfessores.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnProfessoresActionPerformed(evt);
-            }
-        });
+        btnProfessores.addActionListener(evt -> btnProfessoresActionPerformed());
 
         btnAlunos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/Copy v2_32x32.png"))); // NOI18N
         btnAlunos.setText("Alunos");
-        btnAlunos.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAlunosActionPerformed(evt);
-            }
-        });
+        btnAlunos.addActionListener(evt -> btnAlunosActionPerformed());
 
         btnDisciplinas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/book.png"))); // NOI18N
         btnDisciplinas.setText("Disciplinas");
-        btnDisciplinas.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnDisciplinasActionPerformed(evt);
-            }
-        });
+        btnDisciplinas.addActionListener(evt -> btnDisciplinasActionPerformed());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -86,28 +74,28 @@ public class ViewPrincipal extends javax.swing.JFrame {
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAlunosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAlunosActionPerformed
+    private void btnAlunosActionPerformed() {
         ConsultaAlunosView cv = new ConsultaAlunosView();
         cv.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_btnAlunosActionPerformed
+    }
 
-    private void btnDisciplinasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDisciplinasActionPerformed
+    private void btnDisciplinasActionPerformed() {
         ConsultaDisciplinasView cd = new ConsultaDisciplinasView();
         cd.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_btnDisciplinasActionPerformed
+    }
 
-    private void btnProfessoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProfessoresActionPerformed
+    private void btnProfessoresActionPerformed() {
         ConsultaProfessoresView cv = new ConsultaProfessoresView();
         cv.setVisible(true);
         this.dispose();
-    }//GEN-LAST:event_btnProfessoresActionPerformed
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -120,28 +108,15 @@ public class ViewPrincipal extends javax.swing.JFrame {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ViewPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ViewPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ViewPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(ViewPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ViewPrincipal().setVisible(true);
-            }
-        });
+        java.awt.EventQueue.invokeLater(() -> new ViewPrincipal().setVisible(true));
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnAlunos;
-    private javax.swing.JButton btnDisciplinas;
-    private javax.swing.JButton btnProfessores;
     // End of variables declaration//GEN-END:variables
 }
